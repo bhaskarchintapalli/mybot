10fdd78bc65513ef324f84a76189e70ff4737e94
bhaskarchintapalli <41340728+bhaskarchintapalli@users.noreply.github.com>
2020-07-09 19:20:57 +0530

    added express

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '1124178769:AAEcmR77EimqSemALOPeGN2jX2cg47jBMmM';
 const bot = new TelegramBot(token, {polling: true});
 
+var express = require('express');
+var app     = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 bot.on('message', (msg) => {
     
     var Hi = "hi";
