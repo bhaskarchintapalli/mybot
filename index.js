4f0f8b67b4613a32e20711858f213a307b5750e0
bhaskarchintapalli <41340728+bhaskarchintapalli@users.noreply.github.com>
2020-07-09 18:28:40 +0530

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const TelegramBot = require('node-telegram-bot-api'); 
+const token = '1124178769:AAEcmR77EimqSemALOPeGN2jX2cg47jBMmM';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+    
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+    } 
+        
+    });
