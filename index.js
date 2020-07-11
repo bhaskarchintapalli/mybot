416e6334fdc41407d43bde9f1f02c49345583f4a
bhaskarchintapalli <41340728+bhaskarchintapalli@users.noreply.github.com>
2020-07-11 14:11:21 +0530

    added food related

--- index.js
+++ index.js
@@ -14,17 +14,74 @@ app.get('/', function(request, response) {
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
+bot.onText(/\/start/, (msg) => {
+    
+    bot.sendMessage(msg.chat.id, "Welcome", {
+    "reply_markup": {
+        "keyboard": [["Hi"], ["Tinnava"],["Bye"]]
+        }
+    });
+        
+    });
 bot.on('message', (msg) => {
     
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(msg.from.id, "Hello  " + msg.from.first_name);
+        bot.sendMessage(msg.from.id, "Hello  " + msg.from.first_name,
+        {
+            "reply_markup":{
+                "keyboard":[["Em chestunav"],["Tinnava"]]
+            }
+        });
+    }
+    
+    var em="Em chestunav";
+    if(msg.text.indexOf(em)){
+        if((Math.floor(Math.random() * 10))<5){
+            bot.sendMessage(msg.from.id,"Kaali\nnuvu",{
+                "reply_markup":{
+                    "keyboard":[["ala kurchuna"],["kaali"],["Tea tagutuna"]]
+                }
+            })
+
+       }
+       else{
+           bot.sendMessage(msg.from.id,"gaali peelustuna\nnuvu"),{
+               "reply_markup":{
+                   "keyboard":[["Tea tagutuna"],["ala kurchuna"],["kaali"]]
+               }
+           }
+       }
+    }
+    var tea="Tea tagutuna";
+    if(msg.text.indexOf(tea)){
+        bot.sendMessage(msg.chat.id,"Taagu Taagu");
+    }
+
+    var k="kaali";
+    if(msg.text.indexOf(k)){
+        bot.sendMessage(msg.chat.id,"edo okati cheyi\ndabulu oorike ravu");
+    }
+    
+    var a="ala kurchuna";
+    if(msg.text.indexOf(k)){
+        bot.sendMessage(msg.chat.id,"edo okati cheyi\ndabulu oorike ravu");
+    }
+    var tinna="Tinnava";
+    if(msg.text.indexOf(tinna)){
+        bot.sendMessage(msg.chat.id,"tinna\nnuvu",{
+            "reply_markup": {
+                "keyboard":[["Ha"],["Inka ledu"]]
+            }
+        });
     }
 
     var bye = "bye";
 if (msg.text.toString().toLowerCase().includes(bye)) {
 bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
 } 
+
+
         
     });
     var reqTimer = setTimeout(function wakeUp() {
